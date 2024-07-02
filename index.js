a940fd629e18e00b81cfe7076c8876171d68c868
AdrianoVolter <adriano.j.volter@hotmail.com>
2024-07-02 12:31:57 +0000

    menu wallet / test

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.question('Voce prefere A ou B? ', (answer) => {
-    console.log(`Sua resposta foi: ${answer}`);
-    rl.close();
-});
+function menu() {
+  setTimeout(() => {
+    console.clear();
+    console.log("1 - Create Wallet");
+    console.log("2 - Recover Wallet");
+    console.log("3 - Balance");
+    console.log("4 - Send");
+    console.log("5 - Search Tx");
+
+    rl.question("Choose your option: ", (answer) => {
+      switch (answer) {
+        case "1":
+          console.log("Create Wallet");
+          break;
+        case "2":
+          console.log("Recover Wallet");
+          break;
+        case "3":
+          console.log("Balance");
+          break;
+        case "4":
+          console.log("Send");
+          break;
+        case "5":
+          console.log("Search Tx");
+          break;
+        default:
+          console.log("Wrong option");
+          menu();
+      }
+    });
+  }, 1000);
+}
+
+menu();
