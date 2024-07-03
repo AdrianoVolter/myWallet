a90b54310ddba940949781d5fda0df6902a1bf6b
AdrianoVolter <adriano.j.volter@hotmail.com>
2024-07-03 18:04:14 +0000

    recriando as pasta usando nomorepo/ criando as funçoes iniciais

---
+++ packages/server/src/index.js
@@ -0,0 +1,50 @@
+const readline = require("readline");
+require("dotenv").config();
+
+const SYMBOL = process.env.SYMBOL;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function menu() {
+  setTimeout(() => {
+    console.clear();
+    console.log("1 - Create Wallet");
+    console.log("2 - Recover Wallet");
+    console.log("3 - Balance");
+    console.log("4 - Send" + SYMBOL);
+    console.log("5 - Search Tx");
+
+    rl.question("Choose your option: ", (answer) => {
+      switch (answer) {
+        case "1":
+          console.log("Create Wallet");
+          menu();
+          break;
+        case "2":
+          console.log("Recover Wallet");
+          menu();
+          break;
+        case "3":
+          console.log("Balance");
+          menu();
+          break;
+        case "4":
+          console.log("Send");
+          menu();
+          break;
+        case "5":
+          console.log("Search Tx");
+          menu();
+          break;
+        default:
+          console.log("Wrong option");
+          menu();
+      }
+    });
+  }, 1500);
+}
+
+menu();
